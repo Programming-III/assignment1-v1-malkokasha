fab6a8839690abb751eab8f3d31ae009a8b2aacc
malkokasha <malk.okasha@student.giu-uni.de>
2025-11-12 15:29:06 +0200

    completed Mammal.h class

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,13 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
-
+#include <string>
 #include "Animal.h"
-
-//define mammal class here
-
-
-
-
-
-
+class Mammal: Animal{
+    private:
+    string furColor;
+    
+    public:
+    Mammal(string name,int age,bool isHungry,string furColor);
+    ~Mammal();
+};
 #endif
