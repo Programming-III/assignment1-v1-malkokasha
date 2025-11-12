3e6618fc5c4938be0b46a25967e8abbaa4e4afec
malkokasha <malk.okasha@student.giu-uni.de>
2025-11-12 15:27:44 +0200

    completed Bird.h class

--- Bird.h
+++ Bird.h
@@ -1,14 +1,13 @@
 #ifndef BIRD_H
 #define BIRD_H
-
+#include <string>
 #include "Animal.h"
-
-//define class bird here 
-
-
-
-
-
-
-
+class Bird: Animal{
+    private:
+    float wingSpan;
+    
+    public:
+    Bird(string name,int age,bool isHungry,float wingSpan);
+    ~Bird();
+};
 #endif
